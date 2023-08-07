c888fe9ef4e63e4046f0f27409cc077a639190f0
lawlient <jiuanx2011@163.com>
2023-08-07 22:51:39 +0800

    feat: clean diff

--- web/update_hostname.sh
+++ web/update_hostname.sh
@@ -10,15 +10,24 @@ grep $keyword -rnl | grep -Ev `basename $0` | xargs sed -i "s/$old/$1/g"
 
 grep $keyword -rn | grep -Ev "grep|keyword"
 
+# send email to me
+jarvis -m "$1"
 
 # restart web server related to hostname
 
 # 1. restart gateway
 docker-compose -f ./nginx/docker-compose.yml up -d --build
 
+# docker cp ./nginx/conf/default.conf gateway:/etc/nginx/conf.d/
+# docker cp ./nginx/conf/index.html gateway:/etc/nginx/conf.d/
+# docker restart gateway
+
 # 2. restart jarvis_ui
 docker-compose -f ./amis/docker-compose.yml up -d --build
 
+# docker cp ./amis/index.html jarvis_ui:/amis/packages/amis/sdk/
+# docker cp ./amis/jarvis.html jarvis_ui:/amis/packages/amis/sdk/
+# docker restart jarvis_ui
 
 
 
